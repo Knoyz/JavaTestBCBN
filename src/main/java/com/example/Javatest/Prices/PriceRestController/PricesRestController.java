b6c2fc29b6682aefc91c48135a20c636c88226e4
Knoyz <kennynoy98@gmail.com>
2023-04-19 19:43:55 +0200

    Commit Version Final 0.3
    incluido refactorizado projecto para iniciarce mediante LiquidBase, funcion llenar datos eliminada, ya cuando el servidor levanta se llena la tabla mediante la consulta sql presente en InicializarDatos.sql dentro de resources

--- src/main/java/com/example/Javatest/Prices/PriceRestController/PricesRestController.java
+++ src/main/java/com/example/Javatest/Prices/PriceRestController/PricesRestController.java
@@ -1,24 +1,18 @@
 package com.example.Javatest.Prices.PriceRestController;
 
-import com.example.Javatest.Prices.Model.Price;
 import com.example.Javatest.Prices.Repository.PricesRepository;
 import com.example.Javatest.Prices.Service.PricesService;
 import com.example.Javatest.Prices.Utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @RestController
 @RequestMapping("/prices")
 public class PricesRestController {
-
-    @Autowired
-    PricesRepository pricesRepository;
 
     @Autowired
     PricesService pricesService;
@@ -45,24 +39,4 @@ public class PricesRestController {
 
         return new ResponseEntity<>(response, response.getHttpStatus());
     }
-
-    @PostMapping("/llenarTabla")
-    public ResponseEntity<List<Price>> llenarTabla() {
-
-        Price price1 = new Price(1, LocalDateTime.of(2020, 06, 14, 0, 00, 00), LocalDateTime.of(2020, 12, 31, 23, 59, 59), 1, 35455, 0, 35.50, "EUR");
-        Price price2 = new Price(1, LocalDateTime.of(2020, 06, 14, 15, 00, 00), LocalDateTime.of(2020, 06, 14, 18, 30, 00), 2, 35455, 1, 25.45, "EUR");
-        Price price3 = new Price(1, LocalDateTime.of(2020, 06, 15, 00, 00, 00), LocalDateTime.of(2020, 06, 15, 11, 00, 00), 3, 35455, 1, 30.50, "EUR");
-        Price price4 = new Price(1, LocalDateTime.of(2020, 06, 15, 16, 00, 00), LocalDateTime.of(2020, 12, 31, 23, 59, 59), 4, 35455, 1, 38.95, "EUR");
-
-        pricesRepository.save(price1);
-        pricesRepository.save(price2);
-        pricesRepository.save(price3);
-        pricesRepository.save(price4);
-
-        List<Price> lp = pricesRepository.findAll();
-
-        return new ResponseEntity<>(lp, HttpStatus.OK);
-
-    }
-
 }
--- src/test/java/com/example/Javatest/Prices/Service/PriceServiceTest.java
+++ src/test/java/com/example/Javatest/Prices/Service/PriceServiceTest.java
@@ -16,22 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PriceServiceTest {
 
     @Autowired
-    PricesRepository pricesRepository;
-
-    @Autowired
     PricesService pricesService;
-
-    Price price1 = new Price(1, LocalDateTime.of(2020, 06, 14, 00, 00, 00), LocalDateTime.of(2020, 12, 31, 23, 59, 59), 1, 35455, 0, 35.50, "EUR");
-    Price price2 = new Price(1, LocalDateTime.of(2020, 06, 14, 15, 00, 00), LocalDateTime.of(2020, 06, 14, 18, 30, 00), 2, 35455, 1, 25.45, "EUR");
-    Price price3 = new Price(1, LocalDateTime.of(2020, 06, 15, 00, 00, 00), LocalDateTime.of(2020, 06, 15, 11, 00, 00), 3, 35455, 1, 30.50, "EUR");
-    Price price4 = new Price(1, LocalDateTime.of(2020, 06, 15, 16, 00, 00), LocalDateTime.of(2020, 12, 31, 23, 59, 59), 4, 35455, 1, 38.95, "EUR");
-
-    public void initializeData() {
-        pricesRepository.save(price1);
-        pricesRepository.save(price2);
-        pricesRepository.save(price3);
-        pricesRepository.save(price4);
-    }
 
     @Test
     void getFinalPriceByDateProductIdBrandId_Test1(){
@@ -39,7 +24,6 @@ class PriceServiceTest {
         /*
         * Test 1: petición a las 10:00 del día 14 del producto 35455   para la brand 1 (ZARA)
         */
-        initializeData();
 
         PriceDTO priceDTOExpected = new PriceDTO();
         priceDTOExpected.setBrandID(1);
@@ -66,7 +50,6 @@ class PriceServiceTest {
         /*
          * Test 2: petición a las 16:00 del día 14 del producto 35455   para la brand 1 (ZARA)
          */
-        initializeData();
 
         PriceDTO priceDTOExpected = new PriceDTO();
         priceDTOExpected.setBrandID(1);
@@ -93,7 +76,6 @@ class PriceServiceTest {
         /*
         Test 3: petición a las 21:00 del día 14 del producto 35455   para la brand 1 (ZARA)
          */
-        initializeData();
 
         PriceDTO priceDTOExpected = new PriceDTO();
         priceDTOExpected.setBrandID(1);
@@ -120,7 +102,6 @@ class PriceServiceTest {
         /*
         Test 4: petición a las 10:00 del día 15 del producto 35455   para la brand 1 (ZARA)
          */
-        initializeData();
 
         PriceDTO priceDTOExpected = new PriceDTO();
         priceDTOExpected.setBrandID(1);
@@ -147,7 +128,6 @@ class PriceServiceTest {
         /*
         Test 5: petición a las 21:00 del día 16 del producto 35455   para la brand 1 (ZARA)
          */
-        initializeData();
 
         PriceDTO priceDTOExpected = new PriceDTO();
         priceDTOExpected.setBrandID(1);
